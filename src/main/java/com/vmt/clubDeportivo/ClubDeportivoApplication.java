d5e44118f0d54dff69a9f0d014d90fbd9d1a0817
victormartor <31761760+victormartor@users.noreply.github.com>
2018-10-01 13:10:06 +0200

    first commit

---
+++ src/main/java/com/vmt/clubDeportivo/ClubDeportivoApplication.java
@@ -0,0 +1,12 @@
+package com.vmt.clubDeportivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubDeportivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubDeportivoApplication.class, args);
+	}
+}
